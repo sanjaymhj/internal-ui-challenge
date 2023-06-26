4cfdfb773689d3ab1efde4fb462f1fd0acf0e18a
Sanjay Maharjan <snjmhj@gmail.com>
2023-06-26 22:55:11 +0545

    main Add tile and subtitle styles

--- src/week-three/WeekThree.tsx
+++ src/week-three/WeekThree.tsx
@@ -5,7 +5,10 @@ import './style.scss';
 const WeekThree = () => {
   return (
     <div className="week-three">
-      <div className="title">WeekThree</div>
+      <div className="title-container">
+        <h1 className="title">Week 3</h1>
+        <h2 className="sub-title">2023-Jun-26</h2>
+      </div>
       <label className="switch">
         <input type="checkbox" />
         <span className="slider round"></span>
